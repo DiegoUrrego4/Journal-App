467f44d845880fccc27f3892bcf8d19f9e71e59e
Diego Urrego <diegourrg@gmail.com>
2023-03-03 18:13:36 -0500

    Se implementan thunks y firebase providers para la creación y login de usuarios usando firebase y redux. Se prueba funcionalidad con resultado satisfactorio y se limpia un poco el código en configuraciones.

---
+++ src/firebase/providers.js
@@ -0,0 +1,85 @@
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  updateProfile,
+} from 'firebase/auth';
+import { FirebaseAuth } from './config';
+
+const googleProvider = new GoogleAuthProvider();
+export const signInWithGoogle = async () => {
+  try {
+    const result = await signInWithPopup(FirebaseAuth, googleProvider);
+    // const credentials = GoogleAuthProvider.credentialFromResult(result);
+    const { displayName, email, photoURL, uid } = result.user;
+    return {
+      ok: true,
+      //User Info
+      displayName,
+      email,
+      photoURL,
+      uid,
+    };
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+
+    return {
+      ok: false,
+      errorCode,
+      errorMessage,
+    };
+  }
+};
+
+export const registerUserWithEmailAndPassword = async ({
+  email,
+  password,
+  displayName,
+}) => {
+  try {
+    const resp = await createUserWithEmailAndPassword(
+      FirebaseAuth,
+      email,
+      password
+    );
+    const { uid, photoURL } = resp.user;
+    await updateProfile(FirebaseAuth.currentUser, { displayName });
+    return {
+      ok: true,
+      uid,
+      photoURL,
+      email,
+      displayName,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      ok: false,
+      errorMessage: error.message,
+    };
+  }
+};
+
+export const loginWithEmailAndPassword = async ({ email, password }) => {
+  try {
+    const resp = await signInWithEmailAndPassword(
+      FirebaseAuth,
+      email,
+      password
+    );
+    const { displayName, uid, photoURL } = resp.user;
+    return {
+      ok: true,
+      displayName,
+      uid,
+      photoURL,
+    };
+  } catch (error) {
+    return {
+      ok: false,
+      errorMessage: error.message,
+    };
+  }
+};
