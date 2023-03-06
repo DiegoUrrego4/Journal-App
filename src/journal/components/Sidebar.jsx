896acfa624fae7db3bc960713a4e5a8314cb463b
Diego Urrego <diegourrg@gmail.com>
2023-03-06 15:20:25 -0500

    Se crea nuevo componente reutilizable para la generación de items en el sidebar. Se agrega al nuevo componente <SidebarItem /> lógica para manejo de titulos largos.

--- src/journal/components/Sidebar.jsx
+++ src/journal/components/Sidebar.jsx
@@ -1,22 +1,11 @@
-import { TurnedInNot } from '@mui/icons-material';
-import {
-  Box,
-  Divider,
-  Drawer,
-  Grid,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Toolbar,
-  Typography,
-} from '@mui/material';
+import { Box, Divider, Drawer, List, Toolbar, Typography } from '@mui/material';
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { SidebarItem } from './';
 
 export const Sidebar = ({ drawerWidth = 240 }) => {
   const { displayName } = useSelector((state) => state.auth);
+  const { notes } = useSelector((state) => state.journal);
   return (
     <Box
       component='nav'
@@ -37,22 +26,8 @@ export const Sidebar = ({ drawerWidth = 240 }) => {
         </Toolbar>
         <Divider />
         <List>
-          {['Enero', 'Febrero', 'Marzo', 'Abril'].map((text) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  <TurnedInNot />
-                </ListItemIcon>
-                <Grid container>
-                  <ListItemText primary={text} />
-                  <ListItemText
-                    secondary={
-                      'Culpa est ea consectetur ea ea excepteur quis nisi labore veniam.'
-                    }
-                  />
-                </Grid>
-              </ListItemButton>
-            </ListItem>
+          {notes.map((note) => (
+            <SidebarItem key={note.id} {...note} />
           ))}
         </List>
       </Drawer>
