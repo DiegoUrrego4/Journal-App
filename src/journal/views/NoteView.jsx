9f7a77b1983eecf118a0fa6ebf1b3470094cda21
Diego Urrego <diegourrg@gmail.com>
2023-03-07 15:29:42 -0500

    Se agrega helper para subida de archivos a firebase. Se implementa nuevo reducer para el guardado de archivos y se hace la prueba de guardado con un resultado exitoso.

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { SaveOutlined, UploadOutlined } from '@mui/icons-material';
 import { Button, Grid, IconButton, TextField, Typography } from '@mui/material';
 import { useForm } from '../../hooks/useForm';
-import { setActiveNote, startSaveNote } from '../../store/journal';
+import { setActiveNote, startSaveNote, startUploadingFiles } from '../../store/journal';
 import { ImageGallery } from '../components';
 import Swal from 'sweetalert2';
 import 'sweetalert2/dist/sweetalert2.css';
@@ -32,8 +32,7 @@ export const NoteView = () => {
 
   const onFileInputChange = ({ target }) => {
     if (target.files === 0) return;
-    console.log('Subiendo archivos');
-    // dispatch(startUploadingFiles(target.files));
+    dispatch(startUploadingFiles(target.files));
   };
 
   const fileInputRef = useRef();
--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,9 +1,11 @@
 import { collection, doc, setDoc } from 'firebase/firestore/lite';
 import { FirebaseDB } from '../../firebase/config';
+import { fileUpload } from '../../helpers';
 import {
   addNewEmptyNote,
   savingNewNote,
   setActiveNote,
+  setPhotosToActiveNote,
   setSaving,
   updateNote,
 } from './journalSlice';
@@ -38,5 +40,17 @@ export const startSaveNote = () => {
     const docRef = doc(FirebaseDB, `${uid}/journal/notes/${note.id}`);
     await setDoc(docRef, noteToFireStore, { merge: true });
     dispatch(updateNote(note));
+  };
+};
+
+export const startUploadingFiles = (files = []) => {
+  return async (dispatch) => {
+    dispatch(setSaving());
+    const fileUploadPromises = [];
+    for (const file of files) {
+      fileUploadPromises.push(fileUpload(file));
+    }
+    const photosUrls = await Promise.all(fileUploadPromises);
+    dispatch(setPhotosToActiveNote(photosUrls))
   };
 };
