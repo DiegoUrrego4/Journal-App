e60d36d504b95ff263d19b8b7d1e1f2f3a7d97c3
Diego Urrego <diegourrg@gmail.com>
2023-03-08 12:43:21 -0500

    Se inicia con implementación de pruebas en authThunks.

---
+++ tests/store/auth/thunks.test.js
@@ -0,0 +1,15 @@
+import { checkingCredentials } from '../../../src/store/auth';
+import { checkingAuthentication } from '../../../src/store/auth/thunks';
+
+jest.mock('../../../src/firebase/providers');
+
+describe('Pruebas en authThunks', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => jest.clearAllMocks());
+
+  test('debe de invocar el checkingCredentials', async () => {
+    await checkingAuthentication()(dispatch);
+    expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+  });
+});
